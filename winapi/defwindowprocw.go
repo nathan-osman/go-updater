39e27a2434c6808720f9a1ff78e98cb28b134b26
Nathan Osman <nathan@quickmediasolutions.com>
2018-09-22 12:37:21 -0700

    Add DefWindowProcW function.

---
+++ winapi/defwindowprocw.go
@@ -0,0 +1,25 @@
+package winapi
+
+import (
+	"syscall"
+)
+
+const (
+	WM_DESTROY = 0x0002
+	WM_CLOSE   = 0x0010
+)
+
+var (
+	pDefWindowProcW = user32.NewProc("DefWindowProcW")
+)
+
+// DefWindowProcW calls the default window procedure.
+func DefWindowProcW(hwnd syscall.Handle, msg uint32, wparam, lparam uintptr) uintptr {
+	ret, _, _ := pDefWindowProcW.Call(
+		uintptr(hwnd),
+		uintptr(msg),
+		uintptr(wparam),
+		uintptr(lparam),
+	)
+	return uintptr(ret)
+}
