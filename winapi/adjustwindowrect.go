f34811d77639d95c4cde27e6e3594aa134965317
Nathan Osman <nathan@quickmediasolutions.com>
2018-09-22 21:33:16 -0700

    Remove GetWindowRect function and move RECT to adjustwindowrect.go.

--- winapi/adjustwindowrect.go
+++ winapi/adjustwindowrect.go
@@ -8,6 +8,14 @@ var (
 	pAdjustWindowRect = user32.NewProc("AdjustWindowRect")
 )
 
+// RECT defines the coordinates of the upper-left and lower-right corners of a rectangle.
+type RECT struct {
+	Left   int32
+	Top    int32
+	Right  int32
+	Bottom int32
+}
+
 // AdjustWindowRect calculates the required size of the window rectangle.
 func AdjustWindowRect(rect *RECT, style uint32, menu bool) error {
 	var menuInt int32
