f33af1999f5bb74b639a1252286933d4183c571d
Nathan Osman <nathan@quickmediasolutions.com>
2018-09-22 21:34:24 -0700

    Add LOWORD function.

--- winapi/winapi.go
+++ winapi/winapi.go
@@ -8,3 +8,8 @@ var (
 	kernel32 = syscall.NewLazyDLL("kernel32.dll")
 	user32   = syscall.NewLazyDLL("user32.dll")
 )
+
+// LOWORD retrieves the low-order word from the specified value.
+func LOWORD(v uint32) uint16 {
+	return uint16(v)
+}
