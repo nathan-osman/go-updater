a12e4c58419d5e2bff24d7c25c3149f8906bc63b
Nathan Osman <nathan@quickmediasolutions.com>
2018-09-25 18:52:01 -0700

    Add GetDC function.

---
+++ winapi/getdc.go
@@ -0,0 +1,20 @@
+package winapi
+
+import (
+	"syscall"
+)
+
+var (
+	pGetDC = gdi32.NewProc("GetDC")
+)
+
+// GetDC retrieves a handle to a device context for the client area of a specified window.
+func GetDC(hwnd syscall.Handle) (syscall.Handle, error) {
+	ret, _, err := pGetDC.Call(
+		uintptr(hwnd),
+	)
+	if ret == 0 {
+		return 0, err
+	}
+	return syscall.Handle(ret), nil
+}
