dc1b2ee3eeb413eb1ba8d26086d25949eb896ed8
Nathan Osman <nathan@quickmediasolutions.com>
2018-09-25 19:40:23 -0700

    Add ReleaseDC function.

---
+++ winapi/releasedc.go
@@ -0,0 +1,18 @@
+package winapi
+
+import (
+	"syscall"
+)
+
+var (
+	pReleaseDC = gdi32.NewProc("ReleaseDC")
+)
+
+// ReleaseDC releases a device context.
+func ReleaseDC(hwnd, hdc syscall.Handle) bool {
+	ret, _, _ := pReleaseDC.Call(
+		uintptr(hwnd),
+		uintptr(hdc),
+	)
+	return ret != 0
+}
